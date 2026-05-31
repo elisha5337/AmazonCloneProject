62d5204a431ad2dd78cccf5218029989bb014c8e
elisha5337 <elsayearba@gmail.com>
2026-05-31 05:30:18 -0700

    Fix deployment workflows, public directory, and auth logic

--- src/Components/category/Category.jsx
+++ src/Components/category/Category.jsx
@@ -1,17 +1,13 @@
 import React from "react";
-import classes from "./category.module.css";
+import classes from "./Category.module.css";
 import { CategoryInfo } from "./CategoryInfo.jsx";
 import CategoryCard from "./CategoryCard.jsx";
 function Category() {
-  console.log();
-
-  //   const name = "elsaye";
-  // console.log(CategoryImage);
   return (
     <section className={classes.category_container}>
-      {CategoryInfo.map((infos) => {
-        return <CategoryCard data={infos} />;
-      })}
+      {CategoryInfo.map((infos, index) => (
+        <CategoryCard key={index} data={infos} />
+      ))}
     </section>
   );
 }
