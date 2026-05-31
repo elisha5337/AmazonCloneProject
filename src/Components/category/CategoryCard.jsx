ef641478991e35b9251bcc45207cdbd32e484d55
elisha5337 <elsayearba@gmail.com>
2026-05-31 05:32:41 -0700

    Fix case sensitivity for category styles

--- src/Components/category/CategoryCard.jsx
+++ src/Components/category/CategoryCard.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import classes from "./Category.module.css";
+import { Link } from "react-router";
 function CategoryCard({ data }) {
   //console.log(data);
   return (
     <div className={classes.category}>
-      <a href={`/category/${data.name}`}>
+      <Link to={`/category/${data.name}`}>
         <span>
           <h2>{data?.name}</h2>
           <h2>{data?.title}</h2>
         </span>
         <img src={data?.imglink} alt="amazon" />
         <p>shop now</p>
-      </a>
+      </Link>
     </div>
   );
 }
